a7604d290deef213d7ed560df31872edd39edfb6
denise <denise@10.150.88.53>
2024-05-01 18:03:08 -0300

    First

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package principal;
 
-/**
- *
- * @author denise
- */
+import visao.FrmMenuPrincipal;
+
 public class Principal {
-    
+
+    public static void main(String[] args) {
+       
+        FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
+         
+        objetotela.setVisible(true);
+    }    
 }
